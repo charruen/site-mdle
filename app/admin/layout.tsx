29de20d43641e80cf312cbf83ebff536a4846da0
charruen <charruen@gmail.com>
2026-08-02 18:45:25 +0200

    Personnalisation des titres des pages

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Administration — MDLE Jean Perrin',
+  title: 'Administrateur — MDLE Jean Perrin',
   description: 'Panneau de gestion du site de la MDLE',
 }
 
